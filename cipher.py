07ef4dea0f6ae1906d33587ea5e5a3a4767e1d3a
Nick Elias <134450333+NickElias01@users.noreply.github.com>
2023-08-05 13:47:20 -0600

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,1 +1,39 @@
-# add your code here
+cipher_dict = {
+    "a": "f",
+    "b": "g",
+    "c": "h",
+    "d": "i",
+    "e": "j",
+    "f": "k",
+    "g": "l",
+    "h": "m",
+    "i": "n",
+    "j": "o",
+    "k": "p",
+    "l": "q",
+    "m": "r",
+    "n": "s",
+    "o": "t",
+    "p": "u",
+    "q": "v",
+    "r": "w",
+    "s": "x",
+    "t": "y",
+    "u": "z",
+    "v": "a",
+    "w": "b",
+    "x": "c",
+    "y": "d",
+    "z": "e",
+}
+
+plaintext = input("Please enter your sentence: ")
+plaintext = plaintext.lower()
+ciphertext = ""
+
+for char in plaintext:
+    if char in cipher_dict:
+        char = cipher_dict[char]
+    ciphertext += char
+
+print(ciphertext)
